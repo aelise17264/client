b75862abbc25cec8e7d9a829723ae29ef2abc7e9
unknown <aelise17264@gmail.com>
2021-06-08 19:34:58 -0600

    moved files into client folder, deleted app folder, connected app & header components

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header';
+
+export default () => {
+    return (
+        <div>
+            <Header />
+            <h1>New App</h1>
+        </div>
+    );
+}
