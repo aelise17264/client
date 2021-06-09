fc418a1351a37ee8ae6e21ede379db8c7cc53321
unknown <aelise17264@gmail.com>
2021-06-09 13:59:23 -0600

    connected app to backend api app through axios.post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import {AUTH_USER} from './types';
-
+import axios from 'axios';
 //redux thunk allows us to return a diff value type from our action creators
 //we can return an action object or a function
 //if we return a function it will be automatically called w/ a dispatch function
 //Redux promise has to return a promise, thunk we can return whatever we want
     
-export const signup = ({email, password}) => (dispatch) => {
-    
+export const signup = (formProps) => (dispatch) => {
+    axios.post('http://localhost:4000/signup', formProps);
         
 
 };
