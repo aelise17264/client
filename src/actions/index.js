b019cb5edf445796f3cb9145c049113e66c21660
unknown <aelise17264@gmail.com>
2021-06-09 13:28:32 -0600

    created action folder with types and index files, set up skeleton for redux thunk signup function

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import {AUTH_USER} from './types';
+
+//redux thunk allows us to return a diff value type from our action creators
+//we can return an action object or a function
+//if we return a function it will be automatically called w/ a dispatch function
+//Redux promise has to return a promise, thunk we can return whatever we want
+    
+export const signup = ({email, password}) => (dispatch) => {
+    
+        
+
+};
